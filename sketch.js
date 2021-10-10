48c7a0df3f609c3c28dcbcd5744d16a3db57164b
Candyoder <92256127+Candyoder@users.noreply.github.com>
2021-10-10 17:46:41 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+
+console.log("hello");
+
+var a = 15;
+var b  = 10;
+console.log(a+b);
+
+var eye=10;
+var nose=2500;
+console.log(eye*nose);
+
+var name = "YashVeer";
+ var surname = "singh";
+ console.log(name +" "+surname);
